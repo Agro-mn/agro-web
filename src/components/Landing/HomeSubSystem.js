66fc44d39692d93992c04e50ea8de6fe23970f18
mongolneghem@gmail.com <mongolneghem@gmail.com>
2025-05-13 14:40:14 +0800

    subsystems show

--- src/components/Landing/HomeSubSystem.js
+++ src/components/Landing/HomeSubSystem.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { Row, Col, Flex } from 'antd';
 import app from '../../assets/img/system.png';
+import {LogoName1 , ThemeData1 } from '../Landing1/Contents';
+import SystemCard from './SystemCard';
 
 const HomeSubSystem = () => {
 
@@ -26,7 +28,18 @@ const HomeSubSystem = () => {
     // fetchWeather();
     // setWeather(testData);
   }, []);
-
+  const ThemeDataPlatform = {
+    colorPrimary: 'red',
+    colorBackground: 'pink',
+    colorText: 'red',
+    algorithm: true
+  };
+  const ThemeDataLoan = {
+    colorPrimary: 'blue',
+    colorBackground: 'aliceblue',
+    colorText: 'blue',
+    algorithm: true
+  };
   return (
     <div style={style}>
       <Row gutter={[20, 20]}>
@@ -36,45 +49,37 @@ const HomeSubSystem = () => {
       </Row>
       <Flex align='center' justify='center'>
         <Row gutter={[20, 20]} style={{ width: '60vw', marginTop: '50px' }}>
-          <Col span={12}>
-            <img src={app} alt='' style={{ height: '40vh' }} />
-          </Col>
-          <Col span={12}>
-            <div style={{ padding: '20px' }}>
-              <div style={headerStyle}>
-                <span style={{ borderBottom: '3px solid #efd643' }}>Хөдөө аж ажуйн үйлдвэрлэгчдийн нэгдсэн систем</span>
-              </div>
-              <div style={textStyle}>
-                <span style={{ display: 'flex', textIndent: '100px', textAlign: 'justify' }}>Хөдөө аж ажуйн үйлдвэрлэгчдийн нэгдсэн систем аж ажуйн үйлдвэрлэгчдийн нэгдсэн системаж ажуйн үйлдвэрлэгчдийн нэгдсэн системаж ажуйн үйлдвэрлэгчдийн нэгдсэн системаж ажуйн үйлдвэрлэгчдийн нэгдсэн системаж ажуйн үйлдвэрлэгчдийн нэгдсэн систем</span>
-              </div>
-            </div>
-          </Col>
-          <Col span={12}>
-            <img src={app} alt='' style={{ height: '40vh' }} />
-          </Col>
-          <Col span={12}>
-            <div style={{ padding: '20px' }}>
-              <div style={headerStyle}>
-                <span style={{ borderBottom: '3px solid #efd643' }}>Хөдөө аж ажуйн үйлдвэрлэгчдийн нэгдсэн систем</span>
-              </div>
-              <div style={textStyle}>
-                <span style={{ display: 'flex', textIndent: '100px', textAlign: 'justify' }}>Хөдөө аж ажуйн үйлдвэрлэгчдийн нэгдсэн систем аж ажуйн үйлдвэрлэгчдийн нэгдсэн системаж ажуйн үйлдвэрлэгчдийн нэгдсэн системаж ажуйн үйлдвэрлэгчдийн нэгдсэн системаж ажуйн үйлдвэрлэгчдийн нэгдсэн системаж ажуйн үйлдвэрлэгчдийн нэгдсэн систем</span>
-              </div>
-            </div>
-          </Col>
-          <Col span={12}>
-            <img src={app} alt='' style={{ height: '40vh' }} />
-          </Col>
-          <Col span={12}>
-            <div style={{ padding: '20px' }}>
-              <div style={headerStyle}>
-                <span style={{ borderBottom: '3px solid #efd643' }}>Хөдөө аж ажуйн үйлдвэрлэгчдийн нэгдсэн систем</span>
-              </div>
-              <div style={textStyle}>
-                <span style={{ display: 'flex', textIndent: '100px', textAlign: 'justify' }}>Хөдөө аж ажуйн үйлдвэрлэгчдийн нэгдсэн систем аж ажуйн үйлдвэрлэгчдийн нэгдсэн системаж ажуйн үйлдвэрлэгчдийн нэгдсэн системаж ажуйн үйлдвэрлэгчдийн нэгдсэн системаж ажуйн үйлдвэрлэгчдийн нэгдсэн системаж ажуйн үйлдвэрлэгчдийн нэгдсэн систем</span>
-              </div>
-            </div>
-          </Col>
+          <SystemCard
+            headerStyle={headerStyle}
+            textStyle={textStyle}
+            image={app}
+            themeData={ThemeData1}
+            title='Хөдөө аж ажуйн үйлдвэрлэгчдийн нэгдсэн систем'
+            description='Хөдөө аж ажуйн үйлдвэрлэгчдийн нэгдсэн систем аж ажуйн үйлдвэрлэгчдийн нэгдсэн системаж ажуйн үйлдвэрлэгчдийн нэгдсэн системаж ажуйн үйлдвэрлэгчдийн нэгдсэн системаж ажуйн үйлдвэрлэгчдийн нэгдсэн системаж ажуйн үйлдвэрлэгчдийн нэгдсэн систем'
+            logo={<LogoName1 height={'1rem'}/>}
+            siteUrl='www.portal.agro.mn'
+            systemName='portal'
+          />
+         <SystemCard
+            headerStyle={headerStyle}
+            textStyle={textStyle}
+            image={app}
+            themeData={ThemeDataPlatform}
+            title='Хөдөө аж ажуйн зарын нэгдсэн платформ'
+            description='Хөдөө аж ажуйн зарын нэгдсэн платформ Хөдөө аж ажуйн зарын нэгдсэн платформ Хөдөө аж ажуйн зарын нэгдсэн платформ Хөдөө аж ажуйн зарын нэгдсэн платформ'
+            logo={<LogoName1 height={'1rem'}/>}
+            siteUrl='www.platform.agro.mn'
+          />
+          <SystemCard
+            headerStyle={headerStyle}
+            textStyle={textStyle}
+            image={app}
+            themeData={ThemeDataLoan}
+            title='Тариаланч таны онцлогт зориулсан хөдөө аж ахуйн уян хатан зээлийн үйлчилгээ'
+            description='Тариаланч таны онцлогт зориулсан хөдөө аж ахуйн уян хатан зээлийн үйлчилгээ Тариаланч таны онцлогт зориулсан хөдөө аж ахуйн уян хатан зээлийн үйлчилгээ Тариаланч таны онцлогт зориулсан хөдөө аж ахуйн уян хатан зээлийн үйлчилгээ'
+            logo={<LogoName1 height={'1rem'}/>}
+            siteUrl='www.loan.agro.mn'
+          />
         </Row>
       </Flex>
     </div>
