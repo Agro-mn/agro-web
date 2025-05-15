c94dd0af4e2b0bad70f8afc04636112b0346c1f8
mongolneghem@gmail.com <mongolneghem@gmail.com>
2025-05-15 17:35:51 +0800

    dash

--- src/components/Components/Dashboard/Dashboard.js
+++ src/components/Components/Dashboard/Dashboard.js
@@ -10,18 +10,16 @@ function Dashboard() {
     <Row gutter={[32, 32]} >
       <Col xs={24} sm={24} md={24} lg={24} xl={12} xxl={12}>
         <RadialStackedChart />
-      </Col>
-      <Col xs={24} sm={24} md={24} lg={24} xl={12} xxl={12}>
-        <Costbar />
-      </Col>
+      </Col>     
       <Col xs={24} sm={24} md={24} lg={24} xl={12} xxl={12}>
         <PerformancePie />
       </Col>
       <Col xs={24} sm={24} md={24} lg={24} xl={12} xxl={12}>
         <PerformanceMap />
       </Col>
-
-
+      <Col xs={24} sm={24} md={24} lg={24} xl={12} xxl={12}>
+        <Costbar />
+      </Col>
     </Row>
   );
 };
