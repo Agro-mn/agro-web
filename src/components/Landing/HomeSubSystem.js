4a9ba3e1b303db540c58d3bab2c3360f5f9fb8b9
mongolneghem@gmail.com <mongolneghem@gmail.com>
2025-05-19 16:56:07 +0800

    performance

--- src/components/Landing/HomeSubSystem.js
+++ src/components/Landing/HomeSubSystem.js
@@ -45,7 +45,8 @@ const HomeSubSystem = ({ ThemeDataLoan, ThemeDataPlatform}) => {
             image={app}
             themeData={ThemeData1}
             title='Хөдөө аж ажуйн үйлдвэрлэгчдийн нэгдсэн систем'
-            description='AgroMongolia – Хөдөө аж ахуйн үйлдвэрлэлээ технологитой хослуул. Төлөвлө, хяна, ашиг шимээ өсгө.'
+            description={<span>AgroMongolia – Хөдөө аж ахуйн үйлдвэрлэлээ технологитой хослуул. 
+              <br/><br/>Төлөвлө, хяна, ашиг шимээ өсгө.</span>}
             logo={<LogoName1 height={'1rem'}/>}
             siteUrl='www.portal.agro.mn'
             systemName='portal'
@@ -56,7 +57,8 @@ const HomeSubSystem = ({ ThemeDataLoan, ThemeDataPlatform}) => {
             image={app}
             themeData={ThemeDataPlatform}
             title='Хөдөө аж ажуйн зарын нэгдсэн платформ'
-            description='AgroMongolia - Хөдөө аж ахуйн бүх хэрэгцээг нэг дор. Ашигтай зар, амжилттай арилжаа энд эхэлнэ.'
+            description={<span>AgroMongolia - Хөдөө аж ахуйн бүх хэрэгцээг нэг дор. 
+              <br/><br/>Ашигтай зар, амжилттай арилжаа энд эхэлнэ.</span>}
             logo={<LogoName1 height={'1rem'}/>}
             siteUrl='www.platform.agro.mn'
             systemName='platform'
@@ -67,7 +69,8 @@ const HomeSubSystem = ({ ThemeDataLoan, ThemeDataPlatform}) => {
             image={app}
             themeData={ThemeDataLoan}
             title='Хөдөө аж ахуйн зээлийн үйлчилгээ'
-            description='Тариаланч таны онцлогт зориулсан хөдөө аж ахуйн уян хатан зээлийн үйлчилгээ. Таны хөдөлмөрийг дэмжих санхүүгийн шийдэл.'
+            description={<span>Тариаланч таны онцлогт зориулсан хөдөө аж ахуйн уян хатан зээлийн үйлчилгээ. 
+              <br/><br/>Таны хөдөлмөрийг дэмжих санхүүгийн шийдэл.</span>}
             logo={<LogoName1 height={'1rem'}/>}
             siteUrl='www.loan.agro.mn'
           />
