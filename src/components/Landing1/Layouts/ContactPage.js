5dc9316a0332352c7d352c9274d4beb2ff29ce28
bayts <bayrtsogt348@gmail.com>
2025-05-21 00:53:04 +0800

    zasvar

--- src/components/Landing1/Layouts/ContactPage.js
+++ src/components/Landing1/Layouts/ContactPage.js
@@ -31,23 +31,27 @@ function ContactPage(){
         color: '#fff',
         fontWeight: 'bold',
         marginBottom: '1rem',
+        fontFamily: "var(--main-font)",
+        fontSize: '1rem',
       };
     
       const iconStyle = {
         marginRight: '0.5rem',
+        color: '#fff',
       };
     
       const linkStyle = {
         display: 'block',
         marginBottom: '0.5rem',
-        color: '#000',
+        fontFamily: "var(--main-font)",
+        color: '#fff',
       };
     
       const qrStyle = {
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
-        gap: '0.5rem',
+        gap: '2rem',
       };
     
       const qrImageStyle = {
@@ -66,7 +70,7 @@ function ContactPage(){
                 <Row gutter={[40, 20]}>
             <Col xs={24} sm={12} md={6}>
               <div style={colHeaderStyle}><EnvironmentOutlined style={iconStyle} />Хаяг:</div>
-              <div>
+              <div style={{color: '#fff', fontFamily: "var(--main-font)", fontWeight:500  }}>
                 Сүхбаатар дүүрэг, <br />
                 1-р хороо, <br />
                 Централ тауэр, 333 тоот
@@ -75,7 +79,7 @@ function ContactPage(){
 
             <Col xs={24} sm={12} md={6}>
               <div style={colHeaderStyle}><PhoneOutlined style={iconStyle} />Холбоо барих:</div>
-              <div>
+              <div style={{color: '#fff', fontFamily: "var(--main-font)", fontWeight:500 }}>
                 +976 70113344 <br />
                 +976 99063344
               </div>
@@ -83,7 +87,7 @@ function ContactPage(){
 
             <Col xs={24} sm={12} md={6}>
               <div style={colHeaderStyle}><MailOutlined style={iconStyle} />Холбоо барих:</div>
-              <div>
+              <div style={{color: '#fff', fontFamily: "var(--main-font)", fontWeight:500  }}>
                 <a href="mailto:support@agromongolia.mn" style={linkStyle}>
                   support@agromongolia.mn
                 </a>
@@ -96,21 +100,21 @@ function ContactPage(){
             <Col xs={24} sm={12} md={6}>
               <div style={colHeaderStyle}>Апп татах:</div>
               <div style={qrStyle}>
-                <div>
+                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '0.5rem'}}>
                 <img
                   src="https://api.qrserver.com/v1/create-qr-code/?size=100x100&data=https://www.apple.com/app-store/"
                   alt="App Store QR"
                   style={qrImageStyle}
                 />
-                  <div>App Store</div>
+                  <div style={{color: '#fff', fontFamily: "var(--main-font)", fontWeight:500 }}>App Store</div>
                 </div>
-                <div>
+                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '0.5rem'}}>
                 <img
                     src="https://api.qrserver.com/v1/create-qr-code/?size=100x100&data=https://play.google.com/store"
                     alt="Play Store QR"
                     style={qrImageStyle}
                   />
-                  <div>Play Store</div>
+                  <div style={{color: '#fff', fontFamily: "var(--main-font)", fontWeight:500 }}>Play Store</div>
                 </div>
               </div>
             </Col>
--- src/components/Landing1/Layouts/DirectionPage.js
+++ src/components/Landing1/Layouts/DirectionPage.js
@@ -1,39 +1,38 @@
 import React from 'react';
 import { Card, Row, Col, Typography } from 'antd';
-import {
-  
-  ShoppingOutlined,
-  ControlOutlined,
-  AuditOutlined,
-} from '@ant-design/icons';
+import carrot from '../../../assets/icons/carrot.png';  
+import wheat from '../../../assets/icons/wheat-awn.png';
+import cows from '../../../assets/icons/cow-alt.png';
+import tomat from '../../../assets/icons/tomato.png';
+
 
 const { Title, Paragraph } = Typography;
 
 const data = [
   {
-    icon: <ShoppingOutlined style={{ fontSize: 32, color: '#52c41a' }} />,
+    icon: wheat,
     title: 'Үр тариа',
     description: 'Ухаалгаар ургацаа удирд',
   },
   {
-    icon: <ShoppingOutlined style={{ fontSize: 32, color: '#1890ff' }} />,
+    icon: carrot,
     title: 'Хүнсний ногоо',
     description: 'Таны тарьсан ногоо таны гарт',
   },
   {
-    icon: <ControlOutlined style={{ fontSize: 32, color: '#faad14' }} />,
+    icon: tomat,
     title: 'Хүлэмжийн ногоо',
     description: 'Хүлэмжийн ногоог хүссэнээрээ хяна',
   },
   {
-    icon: <AuditOutlined style={{ fontSize: 32, color: '#eb2f96' }} />,
+    icon: cows,
     title: 'Мал аж ахуй',
     description: 'Мал аж ахуйгаа ухаалгаар бүртгэ, ашиг шимээ хүрт',
   },
 ];
 
 const DirectionPage = () => (
-  <div style={{ padding: '0px' }}>
+  <div style={{ padding: '0px', height: '100%' }}>
     <Row gutter={[24, 24]} justify="center">
       {data.map((item, index) => (
         <Col xs={24} sm={12} md={6} key={index}>
@@ -43,11 +42,14 @@ const DirectionPage = () => (
               borderRadius: '16px',
               textAlign: 'center',
               boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+              height: '100%',
             }}
           >
-            <div style={{ marginBottom: 0 }}>{item.icon}</div>
-            <Title level={5}>{item.title}</Title>
-            <Paragraph>{item.description}</Paragraph>
+            <div style={{ marginBottom: 0, height: "20px"}}>
+              <img src={item.icon} alt={item.title} style={{ width: '35px', height: '35px', margin: '10px' }} />
+            </div>
+            <Title level={5} style={{fontFamily: "var(--main-font)"}}>{item.title}</Title>
+            <Paragraph style={{fontFamily: "var(--main-font)"}}>{item.description}</Paragraph>
           </Card>
         </Col>
       ))}
